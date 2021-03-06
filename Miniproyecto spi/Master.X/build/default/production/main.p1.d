12ff3601968f761ec1cbf6b8d2ff4ae092be64c9
Daniel Pineda <daniel-p96@outlook.com>
2021-03-05 21:50:03 -0600

    Correctly name this.

---
+++ Miniproyecto spi/Master.X/build/default/production/main.p1.d
@@ -0,0 +1,5 @@
+build/default/production/main.p1:  \
+main.c  \
+../../libs/lcd.X/lcd.h  \
+../../libs/eusart.X/eusart.h  \
+../../libs/spi.X/spi.h 
---
+++ Miniproyecto spi/Slave1.X/build/default/production/main.p1.d
@@ -0,0 +1,4 @@
+build/default/production/main.p1:  \
+main.c  \
+../../libs/adc.X/adc.h  \
+../../libs/spi.X/spi.h 
